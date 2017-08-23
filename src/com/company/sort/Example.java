b98ec9282e095771a74551361d6907af4228b41d
tt <tiantao@opzoon.com>
2017-08-23 16:16:14 +0800

    commit example of sort algorith

---
+++ src/com/company/sort/Example.java
@@ -0,0 +1,39 @@
+package com.company.sort;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.ArrayList;
+
+public class Example {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello IDEA");
+    }
+
+    public static void sort(Comparable[] a)  {
+
+    }
+
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+    private static void show(Comparable[] a) {
+        for (Comparable comparable : a) {
+            System.out.println(a + " ");
+        }
+    }
+
+    private static boolean isSorted(Comparable[] a){
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i],a[i-1])) return false;
+        }
+        return true;
+    }
+}
