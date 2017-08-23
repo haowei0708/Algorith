e7e593272e3de7689683fb0bdb80e71bea3c62eb
tt <tiantao@opzoon.com>
2017-08-23 15:16:19 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello IDEA");
+    }
+}
