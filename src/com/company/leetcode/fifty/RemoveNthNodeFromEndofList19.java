1cd5f7520839473cad46ef381e817e6ff9fe50b5
haowei <haowei@beeboxes.com>
2018-09-06 17:48:19 +0800

    19.Remove Nth Node From End of List

---
+++ src/com/company/leetcode/fifty/RemoveNthNodeFromEndofList19.java
@@ -0,0 +1,33 @@
+package com.company.leetcode.fifty;
+
+public class RemoveNthNodeFromEndofList19 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode tmp = new ListNode(0);
+        tmp.next = head;
+        int length = 0;
+        ListNode p = tmp;
+        while (p.next != null) {
+            length++;
+            p = p.next;
+        }
+        int pre = length - n;
+        p = tmp;
+        while (pre > 0) {
+            pre--;
+            p = p.next;
+        }
+        p.next = p.next.next;
+        return tmp.next;
+    }
+
+}
